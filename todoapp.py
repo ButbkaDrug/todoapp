b1ed6f3cbcc49d9d141c4c429279a0aeb42f64ec
ButbkaDrug <butbkadrug@gmail.com>
2023-09-12 16:40:30 +0400

    Refactoring fiel formatting

--- todoapp.py
+++ todoapp.py
@@ -8,12 +8,13 @@ import argparse
 from argparse import Namespace
 
 class Task:
-    def __init__(self,
-                 title:str,
-                 desc: str = None,
-                 due: datetime = None,
-                 priority: int = None
-                 ):
+    def __init__(
+        self,
+        title:str,
+        desc: str = None,
+        due: datetime = None,
+        priority: int = None
+    ):
         self.title = title
         self.description = desc
         self.created = datetime.now()
@@ -24,31 +25,19 @@ class Tasklist:
     def __init__(self):
         self.tasks = self.load_data()
 
-    def get_tasks(self) -> [Task]:
-        return self.tasks
-
-    def get_sorted(self, key:str) -> [Task]:
-        ...
-
     def remove_task(self, args:Namespace):
         id = args.id
         if id > len(self.tasks):
             print(f"Task #{id} doesn't exists", file=stdout)
             return
-
         task = self.tasks[id]
-
         self.tasks.remove(task)
-
         self.list_tasks()
-
         self.save_data()
 
     def purge_tasks(self, args:any = None):
         self.tasks = []
-
         self.save_data()
-
         self.list_tasks()
 
     def list_tasks(self, args:Namespace | None = None):
@@ -76,12 +65,10 @@ class Tasklist:
         )
 
         self.save_data()
-
         self.list_tasks(args)
 
     def load_data(self, filename:str = "jar"):
         data  = []
-
         if path.isfile(filename):
             try:
                 with open(filename, "rb") as file:
@@ -113,22 +100,63 @@ def main():
     parser = argparse.ArgumentParser()
 
 
-    subparsers = parser.add_subparsers(help="Tools for working with a list")
+    subparsers = parser.add_subparsers(
+        help="Tools for working with a list"
+    )
 
-    parser_add = subparsers.add_parser("add", help="Adds new task into the list")
+    parser_add = subparsers.add_parser(
+        "add",
+        help="Adds new task into the list"
+    )
 
-    parser_add.add_argument("title", type=str, nargs="*")
-    parser_add.add_argument("-d", "--date", help="Due date if DD.MM.YYYY format")
-    parser_add.add_argument("-e", "--description", type=str, nargs="*")
-    parser_add.add_argument("-p", "--priority", type=int, default=0, help="Priority of the task. Value 0 for normal and 1 for important")
+    parser_add.add_argument(
+        "title",
+        type=str,
+        nargs="*"
+    )
+
+    parser_add.add_argument(
+        "-d",
+        "--date",
+        help="Due date if DD.MM.YYYY format"
+    )
+
+    parser_add.add_argument(
+        "-e",
+        "--description",
+        type=str,
+        nargs="*"
+    )
+
+    parser_add.add_argument(
+        "-p",
+        "--priority",
+        type=int,
+        default=0,
+        help="Priority of the task. Value 0 for normal and 1 for important"
+    )
 
     parser_add.set_defaults(func=tasks.add_task)
 
-    parser_list = subparsers.add_parser("list", help="Lists all the tasks")
-    parser_list.add_argument("-n", "--name", const="tasks", action="store_const", dest="params")
+    parser_list = subparsers.add_parser(
+        "list",
+        help="Lists all the tasks"
+    )
+
+    parser_list.add_argument(
+        "-n",
+        "--name",
+        const="tasks",
+        action="store_const",
+        dest="params"
+    )
+
     parser_list.set_defaults(func=tasks.list_tasks)
 
-    parser_remove = subparsers.add_parser("remove", help="Removes a task from the list")
+    parser_remove = subparsers.add_parser(
+        "remove",
+        help="Removes a task from the list"
+    )
     parser_remove.add_argument("id", type=int)
     parser_remove.set_defaults(func=tasks.remove_task)
     parser_purge = subparsers.add_parser("purge", help="Clears the list")
